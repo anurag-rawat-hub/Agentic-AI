ca464ae9e1586fe3e4e1b89585b0a164a24b3a41
Anurag Rawat <246465749+anurag-rawat-hub@users.noreply.github.com>
2026-05-21 12:52:07 +0530

    added full project

---
+++ RAG/chat.py
@@ -0,0 +1,51 @@
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_qdrant import QdrantVectorStore
+from google import genai
+import os
+
+load_dotenv()
+
+# Gemini Client
+api_key = os.getenv("GOOGLE_API_KEY")
+
+client = genai.Client(
+    api_key=api_key
+)
+
+
+
+# Embeddings
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-001"
+)
+
+vector_db=QdrantVectorStore.from_existing_collection(
+    url="http://localhost:6333",
+    collection_name="learning_rag",
+    embedding=embedding_model,
+)
+
+#Take user input
+user_query=input("Ask something: ")
+
+#similarity search
+#Returns Relevant chunks from the vector DB
+search_results=vector_db.similarity_search(query=user_query)
+
+context="\n\n\n".join([f"Page Content:{result.page_content} \n Page Number: {result.metadata['page_label']} \n File Location: {result.metadata['source']}"
+for result in search_results])
+
+SYSTEM_PROMPT=f"""You are a helpful AI assistant who answers user query based on the available context retrieved from a PDF file alog with page_contents and page number
+You should only answer the user based on the following context and navigate the user to open the right page number to know more.
+
+Context:
+{context}
+"""
+
+response = client.models.generate_content(
+    model="gemini-2.5-flash",
+    contents=f"{SYSTEM_PROMPT}\n\nUser: {user_query}"
+)
+
+print("AI:", response.text)
