fbcab7da8083c8ff5b2eeffa78c83b5c985cc055
Anurag Rawat <246465749+anurag-rawat-hub@users.noreply.github.com>
2026-06-29 00:23:25 +0530

    added langgraph code

--- RAG_queue/main.py
+++ RAG_queue/main.py
@@ -1,8 +1,10 @@
 from dotenv import load_dotenv
+load_dotenv()
+
 from .server import app
 import uvicorn
 
-load_dotenv()
+
 
 def main():
     uvicorn.run(app, port=8000, host="0.0.0.0")
--- langgraph/chat.py
+++ langgraph/chat.py
@@ -1,9 +1,33 @@
 from typing_extensions import TypedDict
 from typing import Annotated
 from langgraph.graph.message import add_messages
-from langgraph.graph import StateGraph
+from langgraph.graph import StateGraph, START, END
 
 class State(TypedDict):
     messages:Annotated[list, add_messages]
 
+def chatbot(state: State):  #Node:just adds a text in pre-existing list
+    print("Inside chatbot node",state)
+    return {"messages":["\n\nHi, this a message from Chatbot Node"]}
+
+def samplenode(state: State):
+    print("Inside sample node",state)
+    return {"message": ["\n\nSample Message Appended"]}
+
 graph_builder = StateGraph(State)
+graph_builder.add_node("chatbot", chatbot)  #We have to tell graph builder that chatbot is a node
+graph_builder.add_node("samplenode", samplenode)
+
+graph_builder.add_edge(START, "chatbot")         #START-->chatbot-->samplenode-->END
+graph_builder.add_edge("chatbot", "samplenode")
+graph_builder.add_edge("samplenode", END)                #Edges
+
+graph=graph_builder.compile()
+
+updated_state=graph.invoke(State({"messages":["Hi,My name is Anurag"]}))
+print("\n\nUpdated_state", updated_state)
+
+#state={"messages": ["Hey there"]}
+#node runs: chatbot(state:["Hey there"])->["Hi,this a message from the ChatBot node"])
+#state={"messages": ["Hey there","Hi,this a message from the ChatBot node"]}
+
